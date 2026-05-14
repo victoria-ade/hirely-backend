262b5fc74a71115a7be7a9dbc10f186dd2f7f869
victoria-ade <oyindamolaadebusuyi@gmail.com>
2026-05-14 10:47:05 +0100

    Created the user model

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ['customer', 'provider'],
+      default: 'customer',
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
