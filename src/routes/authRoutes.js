c0b53fdf5a5ee3ba35260e6c349c64c47a0d7912
victoria-ade <oyindamolaadebusuyi@gmail.com>
2026-07-21 08:59:15 +0100

    fix: remove verifyEmail from auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {register, login, getMe, verifyEmail} = require('../controllers/authController');
-const {protect} = require('../middleware/authMiddleware');
+const { register, login, getMe } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
-router.get('verify-email/:token', verifyEmail);
 
 module.exports = router;
